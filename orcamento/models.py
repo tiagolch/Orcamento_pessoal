1f63b3b6b6a803524711f9e300fe33f23830c575
Tiago Chaves <tiagolch@hotmail.com>
2020-06-07 03:39:49 +0000

    Criado tabela de usuarios

--- orcamento/models.py
+++ orcamento/models.py
@@ -1,3 +1,14 @@
 from django.db import models
 
-# Create your models here.
+class usuario(models.Model):
+    nome = models.CharField(max_length=20, verbose_name='Nome')
+    sobre_nome = models.CharField(max_length=20, verbose_name='Sobre Nome')
+    senha = models.CharField(max_length=10, verbose_name='Senha')
+    receita = models.FloatField(verbose_name='Receita')
+    ativo = models.BooleanField(default=True)
+
+    class Meta:
+        db_table = 'usuario'
+
+    def __str__(self):
+        return self.nome
