e9d569c5d74f6adca6d42f54ac51954954811b8a
Tiago Chaves <tiagolch@hotmail.com>
2020-06-08 15:43:19 +0000

    Adicionado usuarios ao admin

--- orcamento/admin.py
+++ orcamento/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
+from .models import *
 
-# Register your models here.
+@admin.register(usuario)
+class usuarioAdmn(admin.ModelAdmin):
+    list_display = ['nome', 'sobre_nome', 'receita', 'ativo']
+    list_edit = ['ativo']
