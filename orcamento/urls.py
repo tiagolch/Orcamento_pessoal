5693ea6998f8c11f4cc64a333544e4ec4c267ade
Tiago Chaves <tiagolch@hotmail.com>
2020-06-08 23:42:01 +0000

    criado pagina de lista_despesas

--- orcamento/urls.py
+++ orcamento/urls.py
@@ -3,5 +3,6 @@ from . import views
 
 
 urlpatterns = [
-    path('', views.index, name='index')
-]
+    path('', views.index, name='index'),
+    path('lista_despesas', views.lista_despesas, name='lista_despesas'),
+    ]
--- orcamento/views.py
+++ orcamento/views.py
@@ -2,3 +2,8 @@ from django.shortcuts import render
 
 def index(request):
     return render(request, 'orcamento/index.html')
+
+
+
+def lista_despesas(request):
+    return render(request, 'orcamento.lista_despesas.html')
