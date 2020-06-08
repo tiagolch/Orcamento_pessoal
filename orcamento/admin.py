a97359120a346fe08d4378c79ca5c987243b23f7
Tiago Chaves <tiagolch@hotmail.com>
2020-06-08 16:14:19 +0000

    criado tabela despesas

--- orcamento/admin.py
+++ orcamento/admin.py
@@ -1,7 +1,13 @@
 from django.contrib import admin
 from .models import *
 
-@admin.register(usuario)
+@admin.register(usuarios)
 class usuarioAdmn(admin.ModelAdmin):
     list_display = ['nome', 'sobre_nome', 'receita', 'ativo']
     list_edit = ['ativo']
+
+
+@admin.register(categorias)
+class categoriaAdmin(admin.ModelAdmin):
+    list_display = ['categoria', 'ativo']
+    list_edit = ['ativo']   
--- orcamento/models.py
+++ orcamento/models.py
@@ -1,6 +1,6 @@
 from django.db import models
 
-class usuario(models.Model):
+class usuarios(models.Model):
     nome = models.CharField(max_length=20, verbose_name='Nome')
     sobre_nome = models.CharField(max_length=20, verbose_name='Sobre Nome')
     senha = models.CharField(max_length=10, verbose_name='Senha')
@@ -8,7 +8,36 @@ class usuario(models.Model):
     ativo = models.BooleanField(default=True)
 
     class Meta:
-        db_table = 'usuario'
+        db_table = 'usuarios'
 
     def __str__(self):
         return self.nome
+
+
+class categorias(models.Model):
+    categoria = models.CharField(max_length=25, verbose_name='Categoria')
+    ativo = models.BooleanField(default=True)
+
+    class Meta:
+        db_table = 'categorias'
+
+    def __str__(self):
+        return self.categoria
+
+
+
+class despesas(models.Model):
+    usuario = models.ForeignKey(usuarios, on_delete=models.DO_NOTHING , verbose_name='Usuario')
+    categoria = models.ForeignKey(categorias, on_delete=models.DO_NOTHING, verbose_name='Categoria')
+    descricao = models.CharField(max_length=100, verbose_name='Descrição')
+    valor = models.FloatField(verbose_name='Valor')
+    data = models.DateTimeField(auto_now_add=True)
+
+    class Meta:
+        db_table = 'despesas'
+
+    def __str__(self):
+        return str(self.usuario)
+
+    
+
